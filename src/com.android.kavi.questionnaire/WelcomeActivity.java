ee06b809622c004b20f51caf5ca327a486e5cfb4
Kavimal Wijewardana <kavi707@gmail.com>
2015-01-03 13:43:52 +0530

    Add initial project files

---
+++ src/com.android.kavi.questionnaire/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.android.kavi.questionnaire;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kavi707 on 1/3/15.
+ */
+public class WelcomeActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
