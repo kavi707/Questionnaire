4ce0446326ead685c336bd304e07fc3501af317b
Kavimal Wijewardana <kavi707@gmail.com>
2015-02-04 01:50:35 +0530

    Build some initial UIs

---
+++ src/com.android.kavi.questionnaire/QuestionActivity.java
@@ -0,0 +1,18 @@
+package com.android.kavi.questionnaire;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kavi707 on 2/3/15.
+ *
+ * @author Kavimal Wijewardana <kavi707@gmail.com>
+ */
+public class QuestionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
--- src/com.android.kavi.questionnaire/WelcomeActivity.java
+++ src/com.android.kavi.questionnaire/WelcomeActivity.java
@@ -1,15 +1,46 @@
 package com.android.kavi.questionnaire;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.android.kavi.questionnaire.QuestionActivity;
 
 /**
  * Created by kavi707 on 1/3/15.
+ *
+ * @author Kavimal Wijewardana <kavi707@gmail.com>
  */
 public class WelcomeActivity extends Activity {
+
+    private Thread thread;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        thread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized(this){
+                        wait(3000);
+                    }
+                }
+                catch(InterruptedException ex){
+                }
+
+                onContinue();
+            }
+        };
+
+        thread.start();
+    }
+
+    private void onContinue() {
+        Intent questionIntent = new Intent(WelcomeActivity.this, QuestionActivity.class);
+        startActivity(questionIntent);
+        finish();
     }
 }
