21204b9e45c838a92816cb67a6f38c3b5190131f
Kavimal Wijewardana <kavi707@gmail.com>
2015-02-15 13:02:41 +0530

    Commit the changes

--- src/com/android/kavi/questionnaire/activities/QuestionActivity.java
+++ src/com/android/kavi/questionnaire/activities/QuestionActivity.java
@@ -44,7 +44,7 @@ public class QuestionActivity extends Activity {
     private QuestionnaireSQLiteOpenHelper questionnaireSQLiteOpenHelper = new QuestionnaireSQLiteOpenHelper(this);
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
 
